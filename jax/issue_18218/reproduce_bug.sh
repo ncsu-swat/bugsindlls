1ce366c9d66a1088f137dfd91bbeb61fef19a451
abidnazirisami <abidnazirisami@gmail.com>
2024-03-25 16:41:51 -0400

    Added proper command to script of jax issue 18218 to run on CI

--- jax/issue_18218/reproduce_bug.sh
+++ jax/issue_18218/reproduce_bug.sh
@@ -1,6 +1,7 @@
 conda init
 conda create --name issue_18218 python==3.11 pip -y
-source activate issue_18218
+eval "$(conda shell.bash hook)"
+conda activate issue_18218
 pip install jax==0.4.18 -f https://storage.googleapis.com/jax-releases/jax_releases.html
 pip install jaxlib==0.4.18 -f https://storage.googleapis.com/jax-releases/jax_releases.html
 pip install -U pytest
