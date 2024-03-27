ee44f46c820fde053029c65182b1b1a78ffda533
Marcelo d'Amorim <mdamori@ncsu.edu>
2024-03-27 17:50:15 -0400

    simple checker for OS

--- tensorflow/issue_62549/reproduce_bug.sh
+++ tensorflow/issue_62549/reproduce_bug.sh
@@ -1,2 +1,7 @@
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+    echo "Broken assumption: Script was tested only on Linux."
+    exit
+fi
+
 docker build -t issue_62549 .
 docker run -it --rm --gpus all issue_62549
