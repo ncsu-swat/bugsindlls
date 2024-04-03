69e8b70792596af36e30837f64794e72690832af
M M Abid Naziri <mnaziri@ncsu.edu>
2024-04-03 01:44:21 -0400

    Fixed issue no. for 20267 in jax

--- jax/issue_20267/test_issue_20267.py
+++ jax/issue_20267/test_issue_20267.py
@@ -9,7 +9,7 @@ def f(x):
     return inner(0., x)[0]
 
 def test_f():
-    issue_no = '18218'
+    issue_no = '20267'
     print('Jax issue no.', issue_no)
     jax.print_environment_info()
     with pytest.raises(IndexError) as e_info:
