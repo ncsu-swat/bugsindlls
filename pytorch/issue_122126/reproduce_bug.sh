6009376196f91769c58d961acd2491ded4a8843b
M M Abid Naziri <abidnazirisami@gmail.com>
2024-04-04 09:20:02 -0400

    Added requirements.txt to pin dependencies

--- pytorch/issue_122126/reproduce_bug.sh
+++ pytorch/issue_122126/reproduce_bug.sh
@@ -2,9 +2,7 @@ conda init
 conda create --name issue_122126 python==3.10 pip -y
 eval "$(conda shell.bash hook)"
 conda activate issue_122126
-pip3 install --pre torch==2.4.0.dev20240317 --index-url https://download.pytorch.org/whl/nightly
-pip3 install numpy
-pip install -U pytest
+pip install -r requirements.txt
 python -m torch.utils.collect_env
 pytest -sx
 returncode=$?
--- tensorflow/issue_61974/reproduce_bug.sh
+++ tensorflow/issue_61974/reproduce_bug.sh
@@ -2,8 +2,7 @@ conda init
 conda create --name issue_61974 python==3.10 pip -y
 eval "$(conda shell.bash hook)"
 conda activate issue_61974
-pip3 install --no-cache-dir tensorflow==2.13.0
-pip install -U pytest
+pip install -r requirements.txt
 pytest -sx
 returncode=$?
 conda deactivate
