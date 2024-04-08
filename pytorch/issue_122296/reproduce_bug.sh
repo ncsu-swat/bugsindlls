c1b2e27848cc66f217893b56fcdc59b8f1093758
M M Abid Naziri <abidnazirisami@gmail.com>
2024-04-08 05:54:00 -0400

    Added check for nvidia driver version

--- pytorch/issue_122296/reproduce_bug.sh
+++ pytorch/issue_122296/reproduce_bug.sh
@@ -1,11 +1,45 @@
-conda init
-conda create --name issue_122296 python==3.10 pip -y
-eval "$(conda shell.bash hook)"
-conda activate issue_122296
-pip install -r requirements.txt
-python -m torch.utils.collect_env
-pytest -sx
-returncode=$?
-conda deactivate
-conda env remove --name issue_122296 -y
-exit ${returncode}
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+    echo "Broken assumption: Script was tested only on Linux."
+    exit 2
+fi
+
+# Minimum required nvidia driver version:
+reqmajor=525
+reqminor=60
+reqpatch=13
+
+driver_version=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
+
+major=$(echo "$driver_version" | cut -d. -f1)
+minor=$(echo "$driver_version" | cut -d. -f2)
+patch=$(echo "$driver_version" | cut -d. -f3)
+
+nvidiadrivermsg="Broken assumption: Script requires an nvidia driver with a version >=${reqmajor}.${reqminor}.${reqpatch}"
+
+if ! [[ "$major" =~ $re ]]
+then
+    echo ${nvidiadrivermsg}
+    exit 2
+elif [ "$major" -lt "$reqmajor" ]
+then
+    echo ${nvidiadrivermsg}
+    exit 2
+elif [ "$major" -eq "$reqmajor" ]
+then
+    if [ "$minor" -lt "$reqminor" ]
+    then
+        echo ${nvidiadrivermsg}
+        exit 2
+    elif [ "$minor" -eq "$reqminor" ]
+    then
+        if [ "$patch" -lt "$reqpatch" ]
+        then
+            echo ${nvidiadrivermsg}
+            exit 2
+        fi
+    fi
+fi
+
+docker build -t issue_122296 .
+docker run -it --rm --gpus all issue_122296
+exit $?
