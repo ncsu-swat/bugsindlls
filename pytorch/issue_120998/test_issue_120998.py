a05d517fa255018b3659885724b9cbd2099d6d13
M M Abid Naziri <abidnazirisami@gmail.com>
2024-04-08 10:14:02 -0400

    Added bug from issue no. 120998 - pytorch

---
+++ pytorch/issue_120998/test_issue_120998.py
@@ -0,0 +1,16 @@
+import torch
+import pytest
+import os
+from torch._dynamo.exc import BackendCompilerFailed
+
+@torch.compile()
+def call(data):
+	return data.view(torch.uint8) + 1
+
+def test_f():
+    os.system('python -m torch.utils.collect_env')
+    data = torch.randint(0, 2**4, [4096, 4096], device='cuda', dtype=torch.uint8)
+    out = call(data) #OK
+    with pytest.raises(BackendCompilerFailed) as e_info:
+        out = call(data.view(torch.float16)) #Error 
+    print(e_info.value)
