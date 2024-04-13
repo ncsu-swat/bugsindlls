d08fac36f2eb755e98dbc1ba1a15ca3487fd9f6c
M M Abid Naziri <mnaziri@ncsu.edu>
2024-04-13 07:28:14 -0400

    Adjusted reproduction scripts for pytorch to work on macos

--- pytorch/issue_121174/reproduce_bug.sh
+++ pytorch/issue_121174/reproduce_bug.sh
@@ -3,6 +3,10 @@ conda create --name issue_121174 python==3.9 pip -y
 eval "$(conda shell.bash hook)"
 conda activate issue_121174
 pip install -r requirements.txt
+if [[ $OSTYPE == 'darwin'* ]]
+then
+    brew install libomp
+fi
 python -m torch.utils.collect_env
 pytest -sx
 returncode=$?
--- pytorch/issue_122126/reproduce_bug.sh
+++ pytorch/issue_122126/reproduce_bug.sh
@@ -3,6 +3,10 @@ conda create --name issue_122126 python==3.10 pip -y
 eval "$(conda shell.bash hook)"
 conda activate issue_122126
 pip install -r requirements.txt
+if [[ $OSTYPE == 'darwin'* ]]
+then
+    brew install libomp
+fi
 python -m torch.utils.collect_env
 pytest -sx
 returncode=$?
