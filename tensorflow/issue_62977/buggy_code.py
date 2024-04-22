852a5d5e0258dc34d22a74a7c36bd7a714c9c79d
M M Abid Naziri <abidnazirisami@gmail.com>
2024-04-22 06:43:48 -0400

    Added bug from issue no. 62977 - tensorflow

---
+++ tensorflow/issue_62977/buggy_code.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+tf.raw_ops.RecordInput(
+    file_pattern="a",
+    file_random_seed=301,
+    file_shuffle_shift_ratio=0,
+    file_buffer_size=10000,
+    file_parallelism=16,
+    batch_size=-1,
+    compression_type='',
+    name=None)
---
+++ tensorflow/issue_62977/reproduce_bug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+conda init
+conda create --name issue_62977 python==3.11 pip -y
+eval "$(conda shell.bash hook)"
+conda activate issue_62977
+pip install -r requirements.txt
+pytest -sx
+returncode=$?
+conda deactivate
+conda env remove --name issue_62977 -y
+exit ${returncode}
