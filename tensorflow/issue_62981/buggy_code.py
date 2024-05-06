2513655970dfb28bbf5578c638efa24239fca0e7
M M Abid Naziri <mnaziri@ncsu.edu>
2024-05-05 21:55:32 -0400

    Set a seed for tf bug 62981 to combat non-determinism

--- tensorflow/issue_62981/buggy_code.py
+++ tensorflow/issue_62981/buggy_code.py
@@ -4,6 +4,11 @@ import sys
 print('Using tensorflow', tf.__version__)
 print('Using python', sys.version)
 
+seed = 62981
+
+tf.random.set_seed(seed)
+print('Using seed', seed) 
+
 tf.raw_ops.DrawBoundingBoxesV2(
     images=tf.random.normal([1,1,1]),
     boxes=tf.random.normal([1]),
