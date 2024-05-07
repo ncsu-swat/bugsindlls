40dd6bad34da9ca2a302333cdaeac8a3f7fde896
M M Abid Naziri <mnaziri@ncsu.edu>
2024-05-07 19:04:51 -0400

    Ensured the file is deleted before attempting to re-download in bug 60932 - tf

--- tensorflow/issue_60932/reproduce_bug.sh
+++ tensorflow/issue_60932/reproduce_bug.sh
@@ -5,6 +5,7 @@ conda create --name issue_60932 python=3.10 pip -y
 eval "$(conda shell.bash hook)"
 conda activate issue_60932
 pip install -r requirements.txt
+rm ~/.keras/datasets/auto-mpg.csv
 pytest -sx
 returncode=$?
 conda deactivate
