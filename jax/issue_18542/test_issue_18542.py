1f60da1b15c0730a9533498e49653c5f333fc2ca
benWu26 <wu2059@purdue.edu>
2024-05-10 14:58:18 -0700

    Added bug from issue no. 18542 - jax

---
+++ jax/issue_18542/test_issue_18542.py
@@ -0,0 +1,15 @@
+import jax
+import pytest
+import jax.numpy as jnp
+
+def test_f():
+    issue_no = '18542'
+    print('Jax issue no.', issue_no)
+    jax.print_environment_info()
+
+    x = jnp.arange(6).reshape(2, 3)
+    mask = jnp.array([True, True])
+    
+    with pytest.raises(IndexError) as e_info:
+        x[jnp.newaxis, mask] #boolean index did not match shape of indexed array in index 1: got (2,), expected (3,)
+    print(e_info.value)
