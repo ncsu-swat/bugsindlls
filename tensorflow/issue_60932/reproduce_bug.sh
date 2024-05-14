4447b2287511a334d28eee0cd1c104ba7f72653c
TomasAndersonFang <fangsen1996@gmail.com>
2024-05-14 10:12:13 +0800

    Moving OS checking to bash script

--- tensorflow/issue_60932/reproduce_bug.sh
+++ tensorflow/issue_60932/reproduce_bug.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" != "linux"* ]]; then
+    echo "Broken assumption: Script was tested only onte Linux."
+    exit 2
+fi
+
 conda init
 conda create --name issue_60932 python==3.10.6 pip -y
 eval "$(conda shell.bash hook)"
