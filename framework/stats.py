c1834e73eedd9be314736193b02608751b6f2866
M M Abid Naziri <mnaziri@ncsu.edu>
2024-05-15 02:43:29 -0400

    Added option to print stats on buggy files as a trie

--- framework/stats.py
+++ framework/stats.py
@@ -2,9 +2,47 @@ import csv
 import sys
 import os 
 
+## Trie (Implementation taken from https://stackoverflow.com/questions/67769308/how-to-print-trie-in-tree-structure-in-python)
 
-def process_file(libname):
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = {}
+        self.visited = 1
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, array):
+        node = self.root
+        node.visited += 1
+ 
+        for x in array:
+            if x in node.children:
+                node = node.children[x]
+                node.visited += 1
+            else:
+                child = TrieNode()
+                node.children[x] = child
+                node = child            
+        node.is_end = True
+
+    def __repr__(self):
+        def recur(node, indent):
+            return "".join(indent + key + " (" + str(child.visited) + ") " + (" <--|" if child.is_end else "") 
+                                  + recur(child, indent + "  ") 
+                for key, child in node.children.items())
+
+        return recur(self.root, "\n")
+    
+#####################################
+
+def process_file(libname, print_fmt):
+    tr = Trie()
+    print('----------------------------')
     print(libname)
+    print('----------------------------')
     dir_path = os.path.dirname(os.path.realpath(__file__))
     with open(f'{dir_path}/../bug_dataset_{libname}.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(filter(lambda row: not row.startswith(",Filter"), csv_file))
@@ -21,7 +59,12 @@ def process_file(libname):
                 first = False
                 continue
             if row["Reproduced"] == "Yes":
-                print(f'\t{row["Issue #"]} {row["Device"]} {row["Buggy File(s)"]}')
+                if (print_fmt == "rows") or (print_fmt == "both"):
+                    print(f'\t{row["Issue #"]} {row["Device"]} {row["Buggy File(s)"]}')
+
+                if row["Buggy File(s)"] > "":
+                    for file in row["Buggy File(s)"].split(","):
+                        tr.insert(file.split("/"))
 
                 ## cpu/gpu
                 if row["Device"] == "CPU":
@@ -38,11 +81,24 @@ def process_file(libname):
         print(f'  # Require GPU: {num_gpus} ({num_gpus*100/(num_gpus+num_cpus):.2f}%)')
         print(f'  # Require CPU: {num_cpus} ({num_cpus*100/(num_gpus+num_cpus):.2f}%)')        
         print(f'  # C: {num_c} ({num_c*100/(num_c+num_py):.2f}%)')
-        print(f'  # Python: {num_py} ({num_py*100/(num_c+num_py):.2f}%)')                
+        print(f'  # Python: {num_py} ({num_py*100/(num_c+num_py):.2f}%)')
+    
+    if (print_fmt == "trie") or (print_fmt == "both"):
+        print('----------------------------')
+        print(tr)             
+        print('\n----------------------------\n')
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('You need to pass the name of the library. For example, $> python stats.py jax')
         sys.exit(1)
     
-    process_file(sys.argv[1])
+    if len(sys.argv) < 3:
+        # rows: Print rows
+        # trie: Print buggy files in a trie
+        # both: Print the rows AND the trie
+        # none: Do not print lines
+        print("Printing in trie format by default")
+        sys.argv.append("trie")
+    
+    process_file(sys.argv[1], sys.argv[2])
