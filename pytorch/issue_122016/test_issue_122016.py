b50be9977114388eaa2a1c0aedb2eca042b49b6c
TomasAndersonFang <fangsen1996@gmail.com>
2024-05-28 12:18:03 +0800

    Added bug from issue no. 122016 - pytorch

---
+++ pytorch/issue_122016/test_issue_122016.py
@@ -0,0 +1,17 @@
+import torch
+import pytest
+
+
+def f():
+    x = torch.zeros(3, device='mps')
+    x[1] = 1
+    x[2] = 3
+    return x
+
+def test_f():
+    issue_no = '122016'
+    print('PyTorch issue no.', issue_no)
+    
+    result = f()
+
+    assert result[1] == 3  # Work, but should not work since x[1] = 1
