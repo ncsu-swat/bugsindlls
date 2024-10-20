6971c0ef3940ecb8a0b9c528e4397b663f65d829
benWu26 <wu2059@purdue.edu>
2024-10-19 21:29:23 -0400

    Added bug from issue no. 117757 - pytorch

--- pytorch/issue_117757/test_issue_117757.py
+++ pytorch/issue_117757/test_issue_117757.py
@@ -1,26 +1,24 @@
 import unittest
 import torch
+import operator
+import pytest
 
-class TestUnaryFunctions:
+
+class TestUnaryFunctions(unittest.TestCase):
     def test_unary_functions(self):
+        issue_no = '120387'
+        print('Pytorch issue no.', issue_no)
         for op in (operator.abs, ):
-            print("iteration %s" % op)
-            
-            def fn(x, y):
-                return x * op(y)
+                print("iteration %s" % op)
+                def fn(x, y):
+                    return x * op(y)
 
-            arg = torch.ones(4) * 4
-            opt_fn = torch._dynamo.optimize(nopython=True, backend="inductor", dynamic=True)(fn)
-            
-            print("tensor*abs(constant)")
-            
-            # Uncomment the print to see the output
-            # print(opt_fn(arg, -2), fn(arg, -2))
-            
-            # Assert equality to check if the optimization works correctly
-            assert opt_fn(arg, -2) == fn(arg, -2), "Test failed!"
+                arg = torch.ones(4)*4
+                opt_fn = torch._dynamo.optimize(nopython=True, backend="inductor", dynamic=True)(fn)
+                print("tensor*abs(constant)")
 
-if __name__ == "__main__":
-    test = TestUnaryFunctions()
-    test.test_unary_functions()
+                # This fail on second iteration.
+                # tensor([-2., -2., -2., -2.]) VS tensor([2., 2., 2., 2.])
+                # print(opt_fn(arg, -2), fn(arg, -2))
 
+                self.assertFalse(torch.equal(opt_fn(arg, -2), fn(arg, -2)), "Tensors are not equal")
