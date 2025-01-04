d8d1684198a5a7b6f660f1ff3ea4bc8502b4949e
Aman Kumar Singh <amankumarsingh.professional@gmail.com>
2025-01-04 21:43:32 +0530

    fix: Update reproduce_bug.sh and requirements.txt to handle the download and installation of PyTorch CPU-based binaries from Google Drive
    Addresses PyTorch issues: #120188, #120647, #120762, #120803, #121174, #121188, #122126, #122692, #122771, #122842

--- pytorch/issue_120647/reproduce_bug.sh
+++ pytorch/issue_120647/reproduce_bug.sh
@@ -4,6 +4,8 @@ conda init
 conda create --name issue_120647 python==3.10 pip -y
 eval "$(conda shell.bash hook)"
 conda activate issue_120647
+pip install gdown==5.2.0
+gdown --fuzzy https://drive.google.com/file/d/1CBJYB13yJUgz-D2_q-K7YAClkDPKqVcx/view?usp=sharing -O /tmp/
 pip install -r  requirements.txt
 if [[ $OSTYPE == 'darwin'* ]]
 then
--- pytorch/issue_121188/reproduce_bug.sh
+++ pytorch/issue_121188/reproduce_bug.sh
@@ -4,6 +4,8 @@ conda init
 conda create --name issue_121188 python==3.10 pip -y
 eval "$(conda shell.bash hook)"
 conda activate issue_121188
+pip install gdown==5.2.0
+gdown --fuzzy https://drive.google.com/file/d/1PW6DC4pVdnddtKy3YHzRBJwrRKkr7YYx/view?usp=sharing -O /tmp/
 pip install -r  requirements.txt
 if [[ $OSTYPE == 'darwin'* ]]
 then
