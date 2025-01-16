b8668496e4cfce2dd15fc884d106fd797547e8d1
M M Abid Naziri <abidnazirisami@gmail.com>
2025-01-15 19:24:41 -0500

    Fixed errors with already existing clone repo and python command not working in freefuzz docker

--- tool-integration/FreeFuzz/install_freefuzz_docker.sh
+++ tool-integration/FreeFuzz/install_freefuzz_docker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git clone https://github.com/ise-uiuc/FreeFuzz.git
+if [ ! -d FreeFuzz ] ; then
+    git clone https://github.com/ise-uiuc/FreeFuzz.git FreeFuzz
+else
+    cd FreeFuzz
+    git pull https://github.com/ise-uiuc/FreeFuzz.git
+    cd ..
+fi
+
 docker build -t freefuzz .
 docker run --name freefuzz --gpus all -d freefuzz:latest
 docker exec -it freefuzz mongorestore dump/
