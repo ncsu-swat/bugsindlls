ae1e2e5bda5365207ffa8f0262bc5fde90c54ba1
manideepika21 <145089468+manideepika21@users.noreply.github.com>
2025-08-29 18:03:46 -0400

    Update test_issue_57751.py

--- tensorflow/issue_57751/test_issue_57751.py
+++ tensorflow/issue_57751/test_issue_57751.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import pytest
 def test_f():
-    with pytest.raises(ValueError) as e_info:
+    with pytest.raises(tf.errors.InvalidArgumentError) as e_info:
         print('Using tensorflow', tf.__version__)
         with tf.device("cpu"):
             tensor = tf.constant([1, 2, 3, 4, 5])
@@ -12,4 +12,5 @@ def test_f():
             print(tf.experimental.numpy.isreal(tensor).shape)
             print(np.array([True, False, False]).shape)
             print(tf.experimental.numpy.isreal(tensor) == np.array([True, False, False])) # Fail
+
     print(f'{e_info.type.__name__}:{e_info.value}')
