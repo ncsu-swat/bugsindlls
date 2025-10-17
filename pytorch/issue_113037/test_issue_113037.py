c0b10545e8f76a35fcef2f2e00cf8165869d9f4c
manideepika21 <mmyaka21@gmail.com>
2025-10-17 11:26:16 -0400

    Updated test file

--- pytorch/issue_113037/test_issue_113037.py
+++ pytorch/issue_113037/test_issue_113037.py
@@ -1,12 +1,12 @@
 import subprocess
 
-SEGFPE_PROCESS_RETURNCODE = -11
+SEGSEGV_PROCESS_RETURNCODE = -11
 
 def test_f():
     try:     
         subprocess.run(["python3", "-m", "buggy_code.py"], check=True)
     except subprocess.CalledProcessError as err:
         print(err)
-        assert err.returncode == SEGFPE_PROCESS_RETURNCODE
+        assert err.returncode == SEGSEGV_PROCESS_RETURNCODE
     else:
         assert False
