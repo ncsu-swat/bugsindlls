b2b3dbf07b7fe1b24dbf4643ac57b3cb3be5a813
manideepika21 <mmyaka21@gmail.com>
2025-10-21 15:24:13 -0400

    Reproduced pytorch issue_117624

---
+++ pytorch/issue_117624/test_issue_117624.py
@@ -0,0 +1,12 @@
+import subprocess
+
+SIGABRT_PROCESS_RETURNCODE = -6
+
+def test_f():
+    try:     
+        subprocess.run(["python3", "-m", "buggy_code.py"], check=True)
+    except subprocess.CalledProcessError as err:
+        print(err)
+        assert err.returncode == SIGABRT_PROCESS_RETURNCODE
+    else:
+        assert False
