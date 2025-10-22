53ea6d43c229bb1f2297b99ec32dfda00885bcd3
manideepika21 <mmyaka21@gmail.com>
2025-10-22 16:52:20 -0400

    Updated test file eith 2 test cases instead of 1

--- pytorch/issue_104508/test_issue_104508.py
+++ pytorch/issue_104508/test_issue_104508.py
@@ -2,13 +2,15 @@ import torch.nn as nn
 import torch
 import pytest
 
-def test_f():
+def test_f1():
 
     m = nn.ConstantPad3d(padding=1, value=1)
 
     with pytest.raises(RuntimeError) as e_info:  
         m(torch.rand([1,2]))
     print(f'{e_info.type.__name__}: {e_info.value}')
+
+def test_f2():
 
     l = nn.ConstantPad2d(padding=1, value=1)
 
