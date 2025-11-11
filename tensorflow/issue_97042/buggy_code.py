4f43552836583d772680a423edc20176a1c2a982
dvalent2 <dvalent2@ncsu.edu>
2025-11-11 13:10:50 -0500

    test

---
+++ tensorflow/issue_97042/buggy_code.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+
+rng = np.random.default_rng(215)
+
+a = tf.constant(rng.uniform(16211., 1312848., size=(2, 2, 2, 1, 1, 3)), dtype=tf.float32)
+axis = -4
+dtype = tf.int16
+
+with tf.device("/CPU:0"):
+    output_cpu = tf.experimental.numpy.cumsum(a, axis=axis, dtype=dtype)
+
+with tf.device("/GPU:0"):
+    output_gpu = tf.experimental.numpy.cumsum(a, axis=axis, dtype=dtype)
+
+output_np = np.cumsum(a.numpy(), axis=axis, dtype=np.int16)
+print(tf.__version__)   # 2.20.0-dev20250715
+print(output_cpu[0,0,0,0,0,0])  # tf.Tensor(17745, shape=(), dtype=int16)
+print(output_gpu[0,0,0,0,0,0])  # tf.Tensor(32767, shape=(), dtype=int16)
+print(output_np[0,0,0,0,0,0]) # 17745
