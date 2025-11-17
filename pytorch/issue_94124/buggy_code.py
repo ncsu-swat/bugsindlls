f0efc8cc453f9d8eb6feaa904ef872ff26ea4715
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-11-17 11:24:14 -0500

    removed other files

--- pytorch/issue_94124/buggy_code.py
+++
@@ -1,7 +0,0 @@
-import torch
-input = torch.rand([], dtype=torch.float32)
-coefficients = torch.rand([10, 2, 12, 2, 15], dtype=torch.float32)
-res = torch._compute_linear_combination(
-    input=input,
-    coefficients=coefficients,
-)
