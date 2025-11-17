22ee5f14fb85d9c7c5e4fedd7228dae73aa64780
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-11-17 11:35:00 -0500

    removed other files

--- pytorch/issue_94669/buggy_code.py
+++
@@ -1,9 +0,0 @@
-import torch
-input = torch.rand([14, 0, 2423179390303677969, 10, 6], dtype=torch.float32)
-some = False
-compute_uv = True
-res = torch.svd(
-    input=input,
-    some=some,
-    compute_uv=compute_uv,
-)
