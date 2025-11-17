6a18865c07c06ecb1dfbea8d9807b0a4fffc11b8
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-11-17 10:21:39 -0500

    Updated test file

--- tensorflow/issue_57751/test_issue_57751.py
+++ tensorflow/issue_57751/test_issue_57751.py
@@ -2,15 +2,17 @@ import tensorflow as tf
 import numpy as np
 import pytest
 def test_f():
-    with pytest.raises(tf.errors.InvalidArgumentError) as e_info:
-        print('Using tensorflow', tf.__version__)
-        with tf.device("cpu"):
-            tensor = tf.constant([1, 2, 3, 4, 5])
-            print(tf.experimental.numpy.isreal(tensor) == np.array([True, False, False])) # Pass
-        with tf.device("gpu"):
+    print('Using tensorflow', tf.__version__)
+    res = tf.constant(False)
+    with tf.device("cpu"):
+        tensor = tf.constant([1, 2, 3, 4, 5])
+        cpu_res=tf.experimental.numpy.isreal(tensor) == np.array([True, False, False]) # Pass
+        assert cpu_res==res
+        
+    with tf.device("gpu"):
+        with pytest.raises(tf.errors.InvalidArgumentError) as e_info:
             tensor = tf.constant([1, 2, 3, 4, 5])
             print(tf.experimental.numpy.isreal(tensor).shape)
             print(np.array([True, False, False]).shape)
             print(tf.experimental.numpy.isreal(tensor) == np.array([True, False, False])) # Fail
-
-    print(f'{e_info.type.__name__}:{e_info.value}')
+        print(f'{e_info.type.__name__}:{e_info.value}')
