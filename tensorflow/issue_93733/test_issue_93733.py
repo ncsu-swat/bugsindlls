bd1830dd9883c9104b9171d9d410450290726d82
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-11-19 10:53:29 -0500

    Updated return code

--- tensorflow/issue_93733/test_issue_93733.py
+++ tensorflow/issue_93733/test_issue_93733.py
@@ -1,14 +1,14 @@
 import subprocess
 import pytest
 
-SIGABRT_PROCESS_RETURNCODE = -6
+SIGILL_PROCESS_RETURNCODE = -4
 
 def test_f():
     try:
         subprocess.run(["python3", "-m", "buggy_code.py"], check=True)
     except subprocess.CalledProcessError as err:
         print(err)
-        assert err.returncode == SIGABRT_PROCESS_RETURNCODE
+        assert err.returncode == SIGILL_PROCESS_RETURNCODE
     else:
         assert False
 
