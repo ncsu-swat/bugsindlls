e95dc79534786026b16ff702a89f54ede87cbc85
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-11-20 14:45:40 -0500

    Reproduced pytorch issue_60196

---
+++ tensorflow/issue_60196/buggy_code.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+with tf.device("GPU:0"):
+    shape = [94, 47, 76, 127, 90]
+    seed = tf.saturate_cast(tf.random.uniform([2], minval=-1024, maxval=1024, dtype=tf.int64), dtype=tf.int32)
+    alpha = tf.saturate_cast(tf.random.uniform([], minval=-1024, maxval=1024, dtype=tf.int64), dtype=tf.half)
+    res = tf.raw_ops.StatelessRandomGammaV2(
+        shape=shape,
+        seed=seed,
+        alpha=alpha,
+    )
