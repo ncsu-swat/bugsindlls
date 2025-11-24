873cd21315bdaa5faf6444664760449ae2f6cd24
manideepika21 <145089468+manideepika21@users.noreply.github.com>
2025-11-24 12:22:14 -0500

    Update conda environment name for bug reproduction

--- tensorflow/issue_97165/reproduce_bug.sh
+++ tensorflow/issue_97165/reproduce_bug.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 conda init
-conda create --name issue_97125 python==3.11 pip -y
+conda create --name issue_97165 python==3.11 pip -y
 eval "$(conda shell.bash hook)"
-conda activate issue_97125
+conda activate issue_97165
 pip install -r requirements.txt
 pytest -sx
 returncode=$?
 conda deactivate
-conda env remove --name issue_97125 -y
+conda env remove --name issue_97165 -y
 exit ${returncode}
