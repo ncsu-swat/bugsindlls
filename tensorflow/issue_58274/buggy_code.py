a2fe8505ef37d7084aaf335f75861911eda46357
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-11-25 16:03:57 -0500

    Updated buggy_code

--- tensorflow/issue_58274/buggy_code.py
+++ tensorflow/issue_58274/buggy_code.py
@@ -1,16 +1,31 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import sys
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
 import numpy as np
-import pytest
-
-print('Using tensorflow', tf.__version__)
-print('Using python', sys.version)
-
-input = np.random.rand(2, 8, 8, 8)
-x = tf.keras.Input([None, None, 8])
-y = tf.keras.layers.Conv2DTranspose(filters=0,kernel_size=3, padding='same', dilation_rate=(1,1))(x)
-model = tf.keras.Model(x, y)
-z = model(input).numpy()
-print(z.mean())
+print(tf.__version__)
+for _ in range(20):
+    try:
+        seed = 0
+        seed2 = 0
+        shape_0 = 16
+        shape_1 = 9
+        shape_2 = 117
+        shape_3 = 100
+        shape_4 = 71
+        shape_5 = 18
+        shape = [shape_0, shape_1, shape_2, shape_3, shape_4, shape_5, ]
+        means = tf.saturate_cast(tf.random.uniform([], minval=0, maxval=64, dtype=tf.int64), dtype=tf.half)
+        stdevs = tf.saturate_cast(tf.random.uniform([], minval=0, maxval=64, dtype=tf.int64), dtype=tf.half)
+        minvals = tf.saturate_cast(tf.random.uniform([1], minval=0, maxval=64, dtype=tf.int64), dtype=tf.half)
+        maxvals = tf.saturate_cast(tf.random.uniform([], minval=0, maxval=64, dtype=tf.int64), dtype=tf.half)
+        res = tf.raw_ops.ParameterizedTruncatedNormal(
+            seed=seed,
+            seed2=seed2,
+            shape=shape,
+            means=means,
+            stdevs=stdevs,
+            minvals=minvals,
+            maxvals=maxvals,
+        )
+    except:
+        pass
