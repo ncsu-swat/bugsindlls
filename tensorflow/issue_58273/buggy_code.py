af7ab23aa38139718c32e3b4ea35c8dc68b260e5
manideepika21 <145089468+manideepika21@users.noreply.github.com>
2025-11-25 15:45:29 -0500

    Add exception handling to MaxPoolV2 operation
    Wrap MaxPoolV2 call in a try-except block to handle exceptions.

--- tensorflow/issue_58273/buggy_code.py
+++ tensorflow/issue_58273/buggy_code.py
@@ -3,16 +3,19 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
 import numpy as np
 print(tf.__version__)
-padding = "SAME"
-data_format = "NCHW_VECT_C"
-input = tf.random.uniform([1, 1, 16, 2, 1], dtype=tf.float32)
-ksize = [1, 1, 1, 1]
-strides = [1, 1, 1, 1]
-res = tf.raw_ops.MaxPoolV2(
+for _ in range(20):
+    try:
+        padding = "SAME"
+        data_format = "NCHW_VECT_C"
+        input = tf.random.uniform([1, 1, 16, 2, 1], dtype=tf.float32)
+        ksize = [1, 1, 1, 1]
+        strides = [1, 1, 1, 1]
+        res = tf.raw_ops.MaxPoolV2(
             padding=padding,
             data_format=data_format,
             input=input,
             ksize=ksize,
             strides=strides,
         )
-print(res)
+    except:
+        pass
