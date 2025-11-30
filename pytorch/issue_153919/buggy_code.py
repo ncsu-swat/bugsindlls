7dfca0723b4656029c78e9f0af35a8e02a1a76a8
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-11-30 15:13:18 -0500

    Reproduced pytorch issue_153919

--- pytorch/issue_153919/buggy_code.py
+++ pytorch/issue_153919/buggy_code.py
@@ -2,8 +2,10 @@ import torch
 
 print(torch.__version__)
 
-tensor = torch.tensor([-9223372036854775808])
-divisor = torch.tensor(-2)
+tensor = torch.tensor([0, 0, -9223372036854775808,
+        -9223372036854775808, -9223372036854775808, -9223372036854775808,
+        -9223372036854775808, -9223372036854775808, 0,
+        0, 0])
+divisor = torch.tensor(-1)
 
-result=torch.remainder(tensor, divisor)
-print("Result:", result)
+torch.remainder(tensor, divisor)
