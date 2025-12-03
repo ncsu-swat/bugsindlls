6752362a602ec620afa78f1ca2b0a93659e91b84
dvalent2 <dvalent2@ncsu.edu>
2025-12-03 15:09:10 -0500

    tensorflow

---
+++ tensorflow/issue_54415/reproduce_bug.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+    echo "Broken assumption: Script was tested only on Linux."
+    exit 2
+fi
+
+conda init
+conda create --name issue_54415 python=3.8 pip -y
+eval "$(conda shell.bash hook)"
+conda activate issue_54415
+pip install -r requirements.txt
+python -m pytest -sx
+returncode=$?
+conda deactivate
+conda env remove --name issue_54415 -y
+exit ${returncode}
---
+++ tensorflow/issue_54415/test_issue_54415.py
@@ -0,0 +1,18 @@
+import os
+import warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.filterwarnings("ignore")
+
+import tensorflow as tf
+import pytest
+
+def test_f():
+    nbins = -16
+    value_range = [0.0, 5.0]
+    new_values = [-1.0, 0.0, 1.5, 2.0, 5.0, 15]
+    
+    # The bug
+    indices = tf.histogram_fixed_width_bins(new_values, value_range, nbins=nbins)
+    
+    # Assert the bug is reproduced
+    assert indices.numpy().tolist() == [0, 0, 0, 0, 0, 0]
