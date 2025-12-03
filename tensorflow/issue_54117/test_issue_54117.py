77fe52a5476ec3e5c7656e5f9b9af39be6b2387f
dvalent2 <dvalent2@ncsu.edu>
2025-12-03 15:13:39 -0500

    tensorflow

---
+++ tensorflow/issue_54117/test_issue_54117.py
@@ -0,0 +1,11 @@
+import os
+import warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.filterwarnings("ignore")
+
+import tensorflow as tf
+import pytest
+
+def test_f():
+    with pytest.raises(ValueError):
+        tf.keras.metrics.get(None)
