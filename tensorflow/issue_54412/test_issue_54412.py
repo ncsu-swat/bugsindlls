f2e27f305e801c94c4d2f27a96e2b9ea6873a9c5
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-03 21:57:34 -0500

    Updated test file

--- tensorflow/issue_54412/test_issue_54412.py
+++ tensorflow/issue_54412/test_issue_54412.py
@@ -1,8 +1,3 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
-
 import tensorflow as tf
 import pytest
 
@@ -12,6 +7,6 @@ def test_f():
     
     # Bug: tf.boolean_mask accepts non-boolean mask silently
     result = tf.boolean_mask(tensor, mask)
-    
+    print(result)
     # Test passes if bug exists (no error thrown for non-boolean mask)
     assert list(result.numpy()) == [0, 1, 2, 3]
