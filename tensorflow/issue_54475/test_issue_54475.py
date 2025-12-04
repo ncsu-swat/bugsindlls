89a0b1bff3046120a0679083cad2f864f133087e
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-03 21:11:55 -0500

    Updated test file

--- tensorflow/issue_54475/test_issue_54475.py
+++ tensorflow/issue_54475/test_issue_54475.py
@@ -1,12 +1,8 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
-
 import pytest
 import tensorflow as tf
 
 def test_f():
     features = tf.zeros([3, 4], dtype=tf.uint16)
-    with pytest.raises(TypeError):
+    with pytest.raises(TypeError) as e_info:
         tf.nn.gelu(features)
+    print(f'{e_info.type.__name__}: {e_info.value}')
