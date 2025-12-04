17a4f1d146783b7eb1b22d61cf754ff3d1b5f661
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-04 12:50:07 -0500

    Updated test file

--- tensorflow/issue_16192/test_issue_16192.py
+++ tensorflow/issue_16192/test_issue_16192.py
@@ -1,21 +1,16 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
-
 import tensorflow as tf
 import pytest
 
 def test_f():
     y_true = [[0, 1, 0], [0, 0, 1]]
     y_pred = [[0.05, 0.95, 0], [0.1, 0.8, 0.1]]
-
     from_logits = True
     axis = True  
 
-
-    tf.keras.losses.categorical_crossentropy(y_true, y_pred, axis=axis)
-
- 
-    with pytest.raises(AssertionError):
-        tf.keras.losses.categorical_crossentropy(y_true, y_pred, from_logits=from_logits, axis=axis)
+    loss1 = tf.keras.losses.categorical_crossentropy(y_true, y_pred)
+    loss2 = tf.keras.losses.categorical_crossentropy(y_true, y_pred, axis=axis) # Pass
+    print(f'loss1: {loss1.numpy()}, loss2: {loss2.numpy()}')
+    assert True
+    with pytest.raises(AssertionError) as e_info:
+        loss3 = tf.keras.losses.categorical_crossentropy(y_true, y_pred, from_logits=from_logits, axis=axis) #AssertionError
+    print(f'{e_info.type.__name__}: {e_info.value}')
