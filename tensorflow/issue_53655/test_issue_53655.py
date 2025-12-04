a8d82055ae71072c9e977812a17c6640296feeda
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-04 12:11:59 -0500

    Updated test file

--- tensorflow/issue_53655/test_issue_53655.py
+++ tensorflow/issue_53655/test_issue_53655.py
@@ -1,8 +1,3 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
-
 import tensorflow as tf
 import pytest
 
@@ -14,10 +9,12 @@ def test_f():
     segment_ids = [0, 0, 1]
 
     r1 = tf.math.segment_sum(data=data, segment_ids=segment_ids)
-
-    with pytest.raises(tf.errors.InvalidArgumentError):
+    print(f'r1: {r1}')
+    assert True
+    with pytest.raises(tf.errors.InvalidArgumentError) as e_info:
         tf.sparse.segment_sum(
             data=data,
             indices=tf.constant([0, 1, 2]),
             segment_ids=segment_ids
         )
+    print(f'{e_info.type.__name__}: {e_info.value}')
