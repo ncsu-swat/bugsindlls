e78b2b63f1a9aec8e9ab5814ce3cc1788137e4ec
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-03 20:49:54 -0500

    Updated test file

--- tensorflow/issue_54848/test_issue_54848.py
+++ tensorflow/issue_54848/test_issue_54848.py
@@ -1,14 +1,9 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
-
 import tensorflow as tf
 import pytest
 
 def test_f():
     input = 6.0
     result = tf.linalg.tensor_diag_part(input)
-    
+    print(result)
     # The bug: scalar input is accepted, but should fail
     assert result is not None
