c8517805f7dd47c11cc6f65151fca63c6233cc65
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-04 12:29:24 -0500

    Added docker file

--- tensorflow/issue_53581/test_issue_53581.py
+++ tensorflow/issue_53581/test_issue_53581.py
@@ -1,8 +1,3 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
-
 import tensorflow as tf
 import pytest
 
@@ -11,11 +6,10 @@ def test_f():
     partitions = [3, -2, 2, -1, 2]  
     num_partitions = 5
 
+    t1 = tf.ragged.stack_dynamic_partitions(data, partitions, num_partitions) # Succeed
+    print(t1)
+    assert True
     # dynamic_partition 
-    with pytest.raises(tf.errors.InvalidArgumentError):
+    with pytest.raises(tf.errors.InvalidArgumentError) as e_info:
         tf.ragged.stack(tf.dynamic_partition(data, partitions, num_partitions))
-
-    # stack_dynamic_partitions 
-    with pytest.raises(tf.errors.InvalidArgumentError):
-        tf.ragged.stack_dynamic_partitions(data, partitions, num_partitions)
-
+    print(f'{e_info.type.__name__}: {e_info.value}')
