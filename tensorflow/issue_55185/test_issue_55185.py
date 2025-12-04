0e58602c9b66c9edd3657e62b397751dca56d605
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-03 19:32:45 -0500

    Updated test file

--- tensorflow/issue_55185/test_issue_55185.py
+++ tensorflow/issue_55185/test_issue_55185.py
@@ -1,20 +1,15 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'   # suppress TF logs
-warnings.filterwarnings("ignore")          # suppress python warnings
-
-
 import pytest
 import tensorflow as tf
 
 def test_f():
     seed = None
 
-    tf.random.set_seed(seed)
+    seed_res=tf.random.set_seed(seed)
+    assert True
     a = tf.random.uniform([1, 2])
 
-    tf.random.set_seed(seed)
+    seed_res=tf.random.set_seed(seed)
+    assert True
     b = tf.random.uniform([1, 2])
 
-    # TensorFlow returns a boolean tensor, so convert to numpy() before calling .all()
-    assert not tf.equal(a, b).numpy().all(), "Bug not reproduced: a and b are unexpectedly equal"
+    assert not tf.reduce_all(tf.equal(a, b)), "Tensors are equal"
