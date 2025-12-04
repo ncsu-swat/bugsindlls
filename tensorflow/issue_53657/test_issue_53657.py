6b2b9dc0b4d7c189d18064a0ab136ecbdd9c0cf6
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-04 12:00:39 -0500

    Updated test file

--- tensorflow/issue_53657/test_issue_53657.py
+++ tensorflow/issue_53657/test_issue_53657.py
@@ -1,15 +1,13 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
-
 import tensorflow as tf
 import pytest
 
 def test_f():
     logits = tf.random.uniform([16, 1, 10], dtype=tf.float16)
     r1 = tf.nn.softmax(logits, axis=-1)
+    assert True
+    
     logits_sp = tf.sparse.from_dense(logits)
 
-    with pytest.raises(tf.errors.InvalidArgumentError):
+    with pytest.raises(tf.errors.InvalidArgumentError) as e_info:
         tf.sparse.softmax(logits_sp)
+    print(f'{e_info.type.__name__}: {e_info.value}')
