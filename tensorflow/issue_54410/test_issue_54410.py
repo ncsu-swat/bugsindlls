c21a1e73da1e08445c7b2caaf7c7291270bdc4c9
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-03 23:10:55 -0500

    Updated test file

--- tensorflow/issue_54410/test_issue_54410.py
+++ tensorflow/issue_54410/test_issue_54410.py
@@ -1,8 +1,3 @@
-import os
-import warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-warnings.filterwarnings("ignore")
-
 import tensorflow as tf
 import pytest
 
@@ -11,7 +6,12 @@ def test_f():
     axis = 0
     exclusive = -1
     reverse = -1
-
-    res = tf.math.cumsum(x, axis=axis, exclusive=exclusive, reverse=reverse)
-
-    assert list(res.numpy()) == [7.0, 6.0, 4.0, 0.0]
+    
+    res_1 = tf.math.cumsum(x, axis=axis, exclusive=exclusive, reverse=reverse)
+    print(res_1) # tf.Tensor([7. 6. 4. 0.], shape=(4,), dtype=float32)
+    assert list(res_1.numpy()) == [7.0, 6.0, 4.0, 0.0]
+    
+    with pytest.raises(TypeError) as e_info:
+        res_2 = tf.raw_ops.Cumsum(x=x, axis=axis, exclusive=exclusive, reverse=reverse)
+        print(res_2) # TypeError: Expected bool for argument 'exclusive' not -1.
+    print(f'{e_info.type.__name__}: {e_info.value}')
