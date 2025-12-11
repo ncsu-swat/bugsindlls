08f7ce3d197558acb9d9daa82d6ae6d45ffd96c9
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 17:34:27 -0500

    Updated test file

--- pytorch/issue_72900/test_issue_72900.py
+++ pytorch/issue_72900/test_issue_72900.py
@@ -3,13 +3,17 @@ import torch
 import torch.nn as nn
 
 def test_f():
-    # 72900
+
+    results = dict()
     row = 0
     col = 1
     offset = 2
     
-    result = torch.tril_indices(row, col, offset=offset)
+    results["res_1"] = torch.tril_indices(row, col, offset=offset)
+    print(results["res_1"])
+    assert results["res_1"] is not None
     
-    bug_reproduced = result.numel() > 0
-    
-    assert bug_reproduced
+    with pytest.raises(RuntimeError) as e_info:
+        results["res_2"] = torch.triu_indices(row,col,offset=offset)
+        print(results["res_2"])
+    print(f"{e_info.type.__name__}: {e_info.value}")
