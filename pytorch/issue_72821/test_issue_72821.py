18ed349ba7b40f96c38cd89139f0ba70636cbd19
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 16:55:32 -0500

    Updated test file

--- pytorch/issue_72821/test_issue_72821.py
+++ pytorch/issue_72821/test_issue_72821.py
@@ -2,14 +2,50 @@ import pytest
 import torch
 import torch.nn as nn
 
-def test_f():
-    # 72821
-    input_tensor = torch.rand([1, 0, 2, 2])
-    num_features = 5
+def test_f1():
     
-    batchnorm = nn.BatchNorm2d(num_features)
+    a = torch.rand([1, 0, 2])
+    batchnorm = torch.nn.BatchNorm1d(5)
+    res=batchnorm(a)
+    print("BatchNorm1d Result:", res)
+    assert res is not None
 
-    result = batchnorm(input_tensor)
+def test_f2():
+    
+    a = torch.rand([1, 0, 2, 2])
+    batchnorm = torch.nn.BatchNorm2d(5)
+    res=batchnorm(a)
+    print("BatchNorm2d Result:", res)
+    assert res is not None  
 
-    assert isinstance(result, torch.Tensor)
-    assert result.shape == input_tensor.shape
+def test_f3():
+    
+    a = torch.rand([1, 0, 3, 2, 2])
+    batchnorm = torch.nn.BatchNorm3d(5)
+    res=batchnorm(a)
+    print("BatchNorm3d Result:", res)
+    assert res is not None
+
+def test_f4():
+    
+    a = torch.rand([1, 0, 2])
+    instanceNorm = torch.nn.InstanceNorm1d(5)
+    res=instanceNorm(a)
+    print("InstanceNorm1d Result:", res)
+    assert res is not None
+
+def test_f5():
+    
+    a = torch.rand([1, 0, 2, 2])
+    instanceNorm = torch.nn.InstanceNorm2d(5)
+    res=instanceNorm(a)
+    print("InstanceNorm2d Result:", res)
+    assert res is not None
+
+def test_f6():
+    
+    a = torch.rand([1, 0, 3, 2, 2])
+    instanceNorm = torch.nn.InstanceNorm3d(5)
+    res=instanceNorm(a)
+    print("InstanceNorm3d Result:", res)
+    assert res is not None
