efbee2e2fcdca162f5dbdce0814028cf150f77c5
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 18:00:25 -0500

    Updated test file

--- pytorch/issue_72909/test_issue_72909.py
+++ pytorch/issue_72909/test_issue_72909.py
@@ -3,10 +3,12 @@ import torch
 import torch.nn as nn
 
 def test_f():
-    # 72909
-    A = torch.rand([8, 5], dtype=torch.float32)
     
-    with pytest.raises(AssertionError) as excinfo:
+    A = torch.rand([8, 5], dtype=torch.float32)
+    res=torch.pca_lowrank(A)
+    print("Success")
+    assert res is not None
+    with pytest.raises(AssertionError) as e_info:
         torch.svd_lowrank(A)
-        
-    assert "AssertionError" in str(excinfo.type)
+        print("Success")
+    print(f"{e_info.type.__name__}: {e_info.value}")
