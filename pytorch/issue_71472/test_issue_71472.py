903c63324d5b9d5c2339e0190287a65858934468
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 15:23:12 -0500

    Update test file

--- pytorch/issue_71472/test_issue_71472.py
+++ pytorch/issue_71472/test_issue_71472.py
@@ -1,11 +1,26 @@
 import pytest
 import torch
 
-def test_f():
-    # 71472
-    input_tensor = torch.rand([6, 5, 5], dtype=torch.complex128)
+def test_f1():
 
-    with pytest.raises(RuntimeError) as excinfo:
-        torch.nanmean(input_tensor)
-        
-    assert "expected input to have floating point dtype but got ComplexDouble" in str(excinfo.value)
+    input= torch.rand([6, 5, 5], dtype=torch.complex128)
+    mean_res=torch.mean(input)
+    print(mean_res)
+    assert mean_res is not None
+    # tensor(0.5126+0.5373j, dtype=torch.complex128)
+    with pytest.raises(RuntimeError) as e_info:
+        torch.nanmean(input)
+        # RuntimeError: nanmean(): expected input to have floating point dtype but got ComplexDouble
+    print(f"{e_info.type.__name__}: {e_info.value}")
+
+def test_f2():
+
+    input = torch.rand([2, 2], dtype=torch.complex64)
+    input[0] = torch.nan
+    mean_res=torch.mean(input)
+    print(mean_res)
+    assert mean_res is not None
+    with pytest.raises(RuntimeError) as e_info:
+        torch.nanmean(input)
+        # RuntimeError: nanmean(): expected input to have floating point dtype but got ComplexDouble
+    print(f"{e_info.type.__name__}: {e_info.value}")
