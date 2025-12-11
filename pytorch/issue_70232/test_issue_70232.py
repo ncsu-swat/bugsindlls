46b07fd3decc60a1fd2b13fbd06dbe88487c3206
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 00:45:22 -0500

    Updated test file

--- pytorch/issue_70232/test_issue_70232.py
+++ pytorch/issue_70232/test_issue_70232.py
@@ -1,18 +1,28 @@
 import pytest
 import torch
-import torch.nn as nn
 
-def test_f():
-    # Reproduces Bug #70232: AdaptiveAvgPool{2|3}d creates tensor with negative dimension
+def test_f1():
     output_size = [-36, 0]
-    avg_pool = nn.AdaptiveAvgPool2d(output_size)
-    
+    arg_class = torch.nn.AdaptiveAvgPool2d(output_size)
     tensor = torch.rand([128, 2048, 4, 4], dtype=torch.float32)
+    result = arg_class(tensor)
+    print(result.shape)
+    # torch.Size([128, 2048, -36, 0])
+    assert result is not None
+
+def test_f2():
+    output_size = [-36, 0, 0]
+    arg_class = torch.nn.AdaptiveAvgPool3d(output_size)
+    tensor = torch.rand([4, 4, 128, 2048, 4], dtype=torch.float32)
+    result = arg_class(tensor)
+    print(result.shape)
+    # torch.Size([4, 4, -36, 0, 0])
+    assert result is not None
     
-    result = avg_pool(tensor)
-    
-    bug_reproduced = any(dim < 0 for dim in result.shape)
-    
-    assert bug_reproduced, (
-        f"BUG NOT REPRODUCED: The output shape {result.shape} does not contain a negative dimension."
-    )
+def test_f3():
+    m = torch.nn.AdaptiveMaxPool3d((-5,0,0))
+    input = torch.randn(1, 64, 8, 9, 10)
+    with pytest.raises(RuntimeError) as e_info:
+        output = m(input)
+        # RuntimeError: Trying to create tensor with negative dimension -5: [1, 64, -5, 0, 0]
+    print(f"{e_info.type.__name__}: {e_info.value}")
