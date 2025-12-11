6029c15f5f06bb44a18b4af7fd63c01d3e191787
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 14:16:19 -0500

    Updated test file

--- pytorch/issue_71204/test_issue_71204.py
+++ pytorch/issue_71204/test_issue_71204.py
@@ -12,5 +12,4 @@ def test_f():
     with pytest.raises(RuntimeError) as e_info:
         diag_res=torch.diag(a, 3)
         print(diag_res)
-        # RuntimeError: [enforce fail at CPUAllocator.cpp:50] ((ptrdiff_t)nbytes) >= 0. alloc_cpu() seems to have been called with negative number: 18446744073709551608
     print(f"{e_info.type.__name__}: {e_info.value}")
