159d6e244dc83761662b2adbd6903b09f45d3040
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 18:17:14 -0500

    Updated test file

--- pytorch/issue_72910/test_issue_72910.py
+++ pytorch/issue_72910/test_issue_72910.py
@@ -3,10 +3,9 @@ import torch
 import torch.nn as nn
 
 def test_f():
-    # 72909
-    A = torch.rand([8, 5], dtype=torch.float32)
-    
-    with pytest.raises(AssertionError) as excinfo:
-        torch.svd_lowrank(A)
-        
-    assert "AssertionError" in str(excinfo.type)
+    results = dict()
+    input = torch.rand([2, 1])
+    with pytest.raises(RuntimeError) as e_info:
+        torch.fft.irfftn(input)
+        torch.fft.irfft2(input)
+    print(f"{e_info.type.__name__}: {e_info.value}")
