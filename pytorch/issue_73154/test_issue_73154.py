c0c9c1ef64f219ce946ef125be657db46f49e1a8
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 18:34:29 -0500

    Updated test file

--- pytorch/issue_73154/test_issue_73154.py
+++ pytorch/issue_73154/test_issue_73154.py
@@ -1,16 +1,13 @@
 import pytest
 import torch
-import torch.nn.functional as F
 
 def test_f():
-    # 73154
-    input_tensor = torch.rand([1, 1, 2, 2], dtype=torch.float32)
-    indices = torch.randint(-16, 1024, [1, 1, 2, 2], dtype=torch.int64)
+
+    input = torch.rand([1, 1, 2, 2], dtype=torch.float32)
+    indices = torch.randint(-16,1024,[1, 1, 2, 2], dtype=torch.int64)
     kernel_size = [16, -1024]
     stride = [-16, 1]
-    
-    result = F.max_unpool2d(input_tensor, indices, kernel_size, stride)
-
-    bug_reproduced = any(dim < 0 for dim in result.shape)
-    
-    assert bug_reproduced
+    res=torch.nn.functional.max_unpool2d(input, indices, kernel_size, stride)
+    print(res)
+    assert list(res.size()) == [1, 1, 0, -1023]
+    # tensor([], size=(1, 1, 0, -1023))
