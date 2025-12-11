b391f7d1b83b1ce96b16bb00e8f03c66defa2476
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 15:49:53 -0500

    Updated test file

--- pytorch/issue_71610/test_issue_71610.py
+++ pytorch/issue_71610/test_issue_71610.py
@@ -2,11 +2,9 @@ import pytest
 import torch
 
 def test_f():
-    # 71610
-    input_tensor = torch.rand([2, 0, 5, 5], dtype=torch.complex128)
+    input = torch.rand([2, 0, 5, 5], dtype=torch.complex128)
     A = torch.rand([1, 5, 5], dtype=torch.complex128)
 
-    with pytest.raises(RuntimeError) as excinfo:
-        torch.linalg.solve(A, input_tensor)
-
-    assert "INTERNAL ASSERT FAILED" in str(excinfo.value)
+    with pytest.raises(RuntimeError) as e_info:
+        torch.linalg.solve(A, input)
+    print(f"{e_info.type.__name__}: {e_info.value}")
