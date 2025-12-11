a9b9a1c54158ec829e5e947bc4087565dcc711b2
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 14:27:16 -0500

    Updated test file

--- pytorch/issue_71211/test_issue_71211.py
+++ pytorch/issue_71211/test_issue_71211.py
@@ -1,12 +1,14 @@
 import pytest
 import torch
-import torch.nn.functional as F
 
-def test_f():
-    # 71211
-    input_tensor = torch.rand([2, 2])
-    
-    result = F.relu(input_tensor, inplace="aaaa")
-    
-    assert isinstance(result, torch.Tensor)
-    assert result.shape == input_tensor.shape
+def test_f1(): 
+    tensor = torch.rand([2, 2])
+    res_relu=torch.nn.functional.relu(tensor, "aaaa")
+    print(res_relu)
+    assert res_relu is not None
+
+def test_f2(): 
+    tensor = torch.rand([2, 2])
+    max_pool1d_res=torch.nn.functional.max_pool1d(tensor, 1, return_indices="aaa")
+    print(max_pool1d_res)
+    assert max_pool1d_res is not None
