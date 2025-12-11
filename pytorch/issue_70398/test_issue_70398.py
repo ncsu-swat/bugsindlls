d413635d54af20685342cbd878ad594ff67c0042
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 01:27:25 -0500

    Updated test file

--- pytorch/issue_70398/test_issue_70398.py
+++ pytorch/issue_70398/test_issue_70398.py
@@ -1,13 +1,28 @@
 import pytest
 import torch
 
-def test_f():
-    input_tensor = torch.rand([3])
+def test_f1():
+    
+    input = torch.rand([3])
     shape = [-2, 3]
-    
-    res = torch.broadcast_to(input_tensor, shape)
-    
-    with pytest.raises(RuntimeError) as excinfo:
+    res = torch.broadcast_to(input,shape,)
+    print(res.shape)
+    # torch.Size([-2, 3])
+    print(torch.sum(res))
+    # tensor(0.)
+    with pytest.raises(RuntimeError) as e_info:
         torch.all(res)
+        # RuntimeError: Trying to create tensor with negative dimension -2: [-2, 3]
+    print(f"{e_info.type.__name__}: {e_info.value}")
     
-    assert "Trying to create tensor with negative dimension" in str(excinfo.value)
+def test_f2():
+    
+    input = torch.rand([3])
+    shape = [-2, 3]
+    res = input.expand(shape)
+    print(res.shape)
+    print(torch.sum(res))
+    with pytest.raises(RuntimeError) as e_info:
+        torch.all(res)
+        # RuntimeError: Trying to create tensor with negative dimension -2: [-2, 3]
+    print(f"{e_info.type.__name__}: {e_info.value}")
