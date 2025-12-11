40a3156b3d6e9abb4969979dec4df0f462fd405a
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 02:32:44 -0500

    Updated test file

--- pytorch/issue_71058/test_issue_71058.py
+++ pytorch/issue_71058/test_issue_71058.py
@@ -5,8 +5,12 @@ def test_f():
     condition = torch.randint(0, 2, [2, 2], dtype=torch.bool)
     x = torch.rand([2, 2], dtype=torch.float64)
     y = 0.0
-
-    with pytest.raises(TypeError) as excinfo:
-        x.where(condition, y)
+    res=torch.where(condition, x, y)
+    print(res)
+    assert res is not None
+    # tensor([[0.0000, 0.6290],
+    #        [0.0000, 0.0000]], dtype=torch.float64)
     
-    assert "must be Tensor, not float" in str(excinfo.value)
+    with pytest.raises(TypeError) as e_info:
+        print( x.where(condition, y) )
+    print(f"{e_info.type.__name__}: {e_info.value}")
