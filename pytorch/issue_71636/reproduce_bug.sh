9d12b2ac9143cf6d072354b010fb2b773f752a90
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 15:59:36 -0500

    Updated test file

--- pytorch/issue_71636/reproduce_bug.sh
+++ pytorch/issue_71636/reproduce_bug.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
-set -euo pipefail
 
-if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Broken assumption: Script was tested only on Linux."
-    exit 2
+conda init
+conda create --name issue_71636 python==3.9.5 pip -y
+eval "$(conda shell.bash hook)"
+conda activate issue_71636
+pip install -r requirements.txt
+if [[ $OSTYPE == 'darwin'* ]]
+then
+    brew install libomp
 fi
+python -m torch.utils.collect_env
+pytest -sx
+returncode=$?
+conda deactivate
+conda env remove --name issue_71636 -y
+exit ${returncode}
 
-# Minimum NVIDIA driver version
-reqmajor=525
-reqminor=60
-reqpatch=13
-
-driver_version=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
-major=$(echo "$driver_version" | cut -d. -f1)
-minor=$(echo "$driver_version" | cut -d. -f2)
-patch=$(echo "$driver_version" | cut -d. -f3)
-
-nvidiadrivermsg="Broken assumption: Script requires an NVIDIA driver >= ${reqmajor}.${reqminor}.${reqpatch}"
-
-if ! [[ "$major" =~ ^[0-9]+$ ]] || (( major < reqmajor )) || (( major == reqmajor && minor < reqminor )) || (( major == reqmajor && minor == reqminor && patch < reqpatch )); then
-    echo "$nvidiadrivermsg"
-    exit 2
-fi
-
-docker build -t issue_71636 .
-docker run -it --rm --gpus all issue_71636
-exit $?
--- pytorch/issue_71636/test_issue_71636.py
+++ pytorch/issue_71636/test_issue_71636.py
@@ -2,9 +2,13 @@ import pytest
 import torch
 
 def test_f():
-    # 71636
-    input_tensor = torch.randint(-2, 2, [0], dtype=torch.int32)
-    
-    result = torch.median(input_tensor)
-    
-    assert result.item() == -2147483648
+
+    input = torch.randint(-2,2,[0], dtype=torch.int32)
+    median_res=torch.median(input)
+    print(median_res)
+    assert median_res is not None
+    # tensor(-2147483648, dtype=torch.int32)
+    with pytest.raises(RuntimeError) as e_info:
+        torch.min(input)
+        # RuntimeError: min(): Expected reduction dim to be specified for input.numel() == 0. Specify the reduction dim with the 'dim' argument.
+    print(f"{e_info.type.__name__}: {e_info.value}")
