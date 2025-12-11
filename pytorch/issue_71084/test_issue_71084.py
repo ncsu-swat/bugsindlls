e6121e35e40ea968e3cb87433fa1581594cb2a55
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 11:50:27 -0500

    Updated test file

--- pytorch/issue_71084/test_issue_71084.py
+++ pytorch/issue_71084/test_issue_71084.py
@@ -3,9 +3,14 @@ import torch
 import torch.nn.functional as F
 
 def test_f():
-    # 71084
-    input_tensor = torch.rand([0, 1, 2], dtype=torch.float64)
+    results = dict()
+    input = torch.rand([0, 1, 2], dtype=torch.float64)
     
-    result = torch.inverse(input_tensor)
-    
-    assert isinstance(result, torch.Tensor) and result.numel() == 0 and result.dim() == 3
+    with pytest.raises(RuntimeError) as e_info:
+        torch.cholesky_inverse(input)
+        torch.det(input)   
+    print(f"{e_info.type.__name__}: {e_info.value}")
+    res1=torch.cholesky(input)
+    print(res1)
+    res2=torch.inverse(input)
+    print(res2)
