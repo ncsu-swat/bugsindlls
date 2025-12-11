b53158f647e349a7bcf366911b9e506082dbb1c3
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 01:36:14 -0500

    Updated test file

--- pytorch/issue_70486/reproduce_bug.sh
+++ pytorch/issue_70486/reproduce_bug.sh
@@ -1,46 +1,17 @@
-if [[ "$OSTYPE" != "linux-gnu"* ]]; then
-    echo "Broken assumption: Script was tested only on Linux."
-    exit 2
+#!/bin/bash
+
+conda init
+conda create --name issue_70486 python==3.9.5 pip -y
+eval "$(conda shell.bash hook)"
+conda activate issue_70486
+pip install -r requirements.txt
+if [[ $OSTYPE == 'darwin'* ]]
+then
+    brew install libomp
 fi
-
-# Minimum required nvidia driver version:
-reqmajor=525
-reqminor=60
-reqpatch=13
-
-driver_version=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
-
-major=$(echo "$driver_version" | cut -d. -f1)
-minor=$(echo "$driver_version" | cut -d. -f2)
-patch=$(echo "$driver_version" | cut -d. -f3)
-
-nvidiadrivermsg="Broken assumption: Script requires an nvidia driver with a version >=${reqmajor}.${reqminor}.${reqpatch}"
-re='^[0-9]+$'
-
-if ! [[ "$major" =~ $re ]]
-then
-    echo ${nvidiadrivermsg}
-    exit 2
-elif [ "$major" -lt "$reqmajor" ]
-then
-    echo ${nvidiadrivermsg}
-    exit 2
-elif [ "$major" -eq "$reqmajor" ]
-then
-    if [ "$minor" -lt "$reqminor" ]
-    then
-        echo ${nvidiadrivermsg}
-        exit 2
-    elif [ "$minor" -eq "$reqminor" ]
-    then
-        if [ "$patch" -lt "$reqpatch" ]
-        then
-            echo ${nvidiadrivermsg}
-            exit 2
-        fi
-    fi
-fi
-
-docker build -t issue_70486 .
-docker run -it --rm --gpus all issue_70486
-exit $?
+python -m torch.utils.collect_env
+pytest -sx
+returncode=$?
+conda deactivate
+conda env remove --name issue_70486 -y
+exit ${returncode}
--- pytorch/issue_70486/test_issue_70486.py
+++ pytorch/issue_70486/test_issue_70486.py
@@ -6,7 +6,6 @@ def test_f():
     arg_2 = torch.rand([5, 5], dtype=torch.float64)
     arg_3 = torch.rand([1, 5], dtype=torch.complex128)
     
-    with pytest.raises(RuntimeError) as excinfo:
-        torch.addcmul(arg_1, arg_2, arg_3)
-    
-    assert "INTERNAL ASSERT FAILED" in str(excinfo.value)
+    with pytest.raises(RuntimeError) as e_info:
+        res = torch.addcmul(arg_1,arg_2,arg_3)
+    print(f"{e_info.type.__name__}: {e_info.value}")
