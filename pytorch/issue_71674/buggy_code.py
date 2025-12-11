5f5e7d85ad9a62a1f4550bc7f4823362b1b7e538
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 16:20:36 -0500

    Updated test file

---
+++ pytorch/issue_71674/buggy_code.py
@@ -0,0 +1,4 @@
+import torch
+d = torch.complex64
+torch.set_default_dtype(d)
+# segmentation fault
--- pytorch/issue_71674/test_issue_71674.py
+++ pytorch/issue_71674/test_issue_71674.py
@@ -1,11 +1,13 @@
+import subprocess
 import pytest
-import torch
+import signal
 
 def test_f():
-    # 71674
-    d = torch.complex64
-    
-    with pytest.raises(RuntimeError) as excinfo:
-        torch.set_default_dtype(d)
-        
-    assert "only floating-point types are supported" in str(excinfo.value) or "invalid dtype object" in str(excinfo.value)
+
+    try:
+        subprocess.run(["python3", "-m", "buggy_code.py"], check=True)
+    except subprocess.CalledProcessError as err:
+        print(err)
+        assert err.returncode == -signal.SIGSEGV
+    else:
+        assert False
