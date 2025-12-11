858c184dc5f0772a578faaf6c0e9f81879167a99
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 17:17:20 -0500

    Updated test file

--- pytorch/issue_72824/test_issue_72824.py
+++ pytorch/issue_72824/test_issue_72824.py
@@ -2,14 +2,33 @@ import pytest
 import torch
 import torch.nn as nn
 
-def test_f():
-    # 72824
+def test_f1():
+
+    results = dict()
     a = torch.rand([3], dtype=torch.float32)
     b = torch.rand([3], dtype=torch.float64)
-    
-    bce_loss = nn.BCELoss()
+    res=torch.nn.BCEWithLogitsLoss()(a, b)
+    print(res)
+    assert res is not None
 
-    with pytest.raises(RuntimeError) as excinfo:
-        bce_loss(a, b)
-        
-    assert "Found dtype Double but expected Float" in str(excinfo.value)
+    with pytest.raises(RuntimeError) as e_info:
+        torch.nn.BCELoss()(a, b)
+    print(f"{e_info.type.__name__}: {e_info.value}")
+
+def test_f2():
+
+    results = dict()
+    a = torch.rand([3,4], dtype=torch.float32)
+    b = torch.rand([3,4], dtype=torch.float64)
+    res=torch.nn.MultiLabelSoftMarginLoss()(a, b)
+    print(res)
+    assert res is not None
+
+def test_f3():
+
+    results = dict()
+    a = torch.rand([3], dtype=torch.float32)
+    b = torch.rand([3], dtype=torch.float64)
+    res=torch.nn.PoissonNLLLoss()(a, b)
+    print(res)
+    assert res is not None
