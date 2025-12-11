e42c06c5f0fe6a16618eed256bba58670d0929cd
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 14:38:52 -0500

    Updated test file

--- pytorch/issue_71384/test_issue_71384.py
+++ pytorch/issue_71384/test_issue_71384.py
@@ -2,14 +2,28 @@ import pytest
 import torch
 import torch.nn.functional as F
 
-def test_f():
-    # 71384
+def test_f1():
+
     A = torch.rand([0, 4, 4, 3, 0], dtype=torch.float64)
     B = torch.rand([0, 3, 3], dtype=torch.float64)
+    tensorsolve_res=torch.linalg.tensorsolve(A, B)
+    print(tensorsolve_res)
+    assert tensorsolve_res is not None
+    # tensor([], size=(3, 0), dtype=torch.float64)
+    x = torch.linalg.tensorinv(A)
+    with pytest.raises(RuntimeError) as e_info:
+        torch.tensordot(x, B)
+    print(f"{e_info.type.__name__}: {e_info.value}")
     
-    X = torch.linalg.tensorinv(A)
+def test_f2():
+
+    A= torch.rand([6, 4, 4, 3, 2], dtype=torch.float64)
+    B= torch.rand([6, 4, 2], dtype=torch.float64)
+    x = torch.linalg.tensorinv(A)
+    tensordot_res=torch.tensordot(x, B)
+    print("success")
+    assert tensordot_res is not None
     
-    with pytest.raises(RuntimeError) as excinfo:
-        torch.tensordot(X, B)
-        
-    assert "contracted dimensions need to match" in str(excinfo.value)
+    with pytest.raises(RuntimeError) as e_info:
+        print(torch.linalg.tensorsolve(A, B))
+    print(f"{e_info.type.__name__}: {e_info.value}")
