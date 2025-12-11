165439e692dda3bc628dbb897a3e318104826174
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 01:42:21 -0500

    Updated test file

--- pytorch/issue_70486/test_issue_70486.py
+++ pytorch/issue_70486/test_issue_70486.py
@@ -1,10 +1,19 @@
 import pytest
 import torch
 
-def test_f():
+def test_f1():
     arg_1 = torch.rand([5, 5], dtype=torch.float64)
     arg_2 = torch.rand([5, 5], dtype=torch.float64)
     arg_3 = torch.rand([1, 5], dtype=torch.complex128)
+    
+    with pytest.raises(RuntimeError) as e_info:
+        res = torch.addcmul(arg_1,arg_2,arg_3)
+    print(f"{e_info.type.__name__}: {e_info.value}")
+
+def test_f2():
+    arg_1 = torch.rand([1], dtype=torch.complex128)
+    arg_2 = torch.rand([5, 5, 1], dtype=torch.complex128)
+    arg_3 = torch.rand([1, 3], dtype=torch.float32)
     
     with pytest.raises(RuntimeError) as e_info:
         res = torch.addcmul(arg_1,arg_2,arg_3)
