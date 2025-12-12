1059b8565dc3a1c03c5eb32c096d7e8f980eb4bd
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 21:00:51 -0500

    Updated test file

--- pytorch/issue_73504/test_issue_73504.py
+++ pytorch/issue_73504/test_issue_73504.py
@@ -1,14 +1,13 @@
 import pytest
 import torch
-import torch.nn.functional as F
-import torch.nn as nn
 
 def test_f():
-    # 73504
-    input_tensor = torch.randint(0, 4, [], dtype=torch.uint8)
+    results = dict()
+    input = torch.randint(0,4,[], dtype=torch.uint8)
     value = 2110
-    
-    with pytest.raises(RuntimeError) as excinfo:
-        torch.ge(input_tensor, value)
-        
-    assert "value cannot be converted to type uint8_t without overflow" in str(excinfo.value)
+    res=torch.div(input, value, rounding_mode="trunc")
+    print(res)
+    # tensor(0, dtype=torch.uint8)
+    with pytest.raises(RuntimeError) as e_info:
+        torch.ge(input, value)
+    print(f"{e_info.type.__name__}: {e_info.value}")
