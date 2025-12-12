b8e2d775e5229da96db30257b25c55fe5175140e
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 20:09:55 -0500

    Updated test file

--- pytorch/issue_71629/test_issue_71629.py
+++ pytorch/issue_71629/test_issue_71629.py
@@ -1,13 +1,11 @@
 import pytest
 import torch
-import torch.nn.functional as F
-import torch.nn as nn
 
 def test_f():
-    # 71629
-    input_tensor = torch.rand([5, 5, 0])
-
-    with pytest.raises(RuntimeError) as excinfo:
-        torch.max(input_tensor)
-        
-    assert "Expected reduction dim to be specified for input.numel() == 0" in str(excinfo.value)
+    input = torch.rand([5, 5, 0])
+    res=torch.median(input)
+    print(res)
+    assert torch.isnan(res)
+    with pytest.raises(RuntimeError) as e_info:
+        torch.max(input)
+    print(f"{e_info.type.__name__}: {e_info.value}")
