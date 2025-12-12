51b3036446acbdaf30ef5e203f02d92ca40ebbb0
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 20:22:28 -0500

    Updated test file

--- pytorch/issue_73501/test_issue_73501.py
+++ pytorch/issue_73501/test_issue_73501.py
@@ -4,13 +4,12 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 def test_f():
-    # 73501
     input_tensor = torch.rand([], dtype=torch.float64)
     dim = 0
     index = torch.tensor(-1, dtype=torch.int64)
     value = torch.rand([], dtype=torch.float64)
-
-    with pytest.raises(IndexError) as excinfo:
+    res=input_tensor.clone().index_fill(dim, index, value)
+    print(res)
+    with pytest.raises(IndexError) as e_info:
         input_tensor.clone().index_copy(dim, index, value)
-
-    assert "is out of bounds for dimension 0 with size 1" in str(excinfo.value)
+    print(f"{e_info.type.__name__}: {e_info.value}")
