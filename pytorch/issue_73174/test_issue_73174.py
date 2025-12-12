3649ee6366a787918c7c15c117a60cd02bb7d884
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 19:03:38 -0500

    Updated test file

--- pytorch/issue_73174/test_issue_73174.py
+++ pytorch/issue_73174/test_issue_73174.py
@@ -1,16 +1,10 @@
 import pytest
 import torch
-import torch.nn.functional as F
-import torch.nn as nn
 
 def test_f():
-    # 73174
     normalized_shape = [1024]
-    layer = nn.LayerNorm(normalized_shape)
-    
-    input_tensor = torch.randint(0, 8, [0, 1, 1024], dtype=torch.long)
-    
-    with pytest.raises(RuntimeError) as excinfo:
-        layer(input_tensor)
-        
-    assert "INTERNAL ASSERT FAILED" in str(excinfo.value)
+    layer = torch.nn.LayerNorm(normalized_shape)
+    input = torch.randint(0,8, [0, 1, 1024])
+    with pytest.raises(RuntimeError) as e_info:
+        res = layer(input)
+    print(f"{e_info.type.__name__}: {e_info.value}")
