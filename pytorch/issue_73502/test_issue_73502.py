7c5c2887f9f86ee81c84d441e8e6cd6eaaefe1ce
Manideepika Reddy Myaka <mmyaka@mdamori-ub01.csc.ncsu.edu>
2025-12-11 20:34:51 -0500

    Updated test file

--- pytorch/issue_73502/test_issue_73502.py
+++ pytorch/issue_73502/test_issue_73502.py
@@ -1,13 +1,9 @@
 import pytest
 import torch
-import torch.nn.functional as F
-import torch.nn as nn
 
 def test_f():
-    # 73502
-    input_tensor = torch.rand([1, 1], dtype=torch.complex32)
-    
-    with pytest.raises(RuntimeError) as excinfo:
-        input_tensor.storage()
-        
-    assert "unsupported Storage type" in str(excinfo.value)
+
+    input = torch.rand([1, 1], dtype=torch.complex32)
+    with pytest.raises(RuntimeError) as e_info:
+        input.storage()
+    print(f"{e_info.type.__name__}: {e_info.value}")
